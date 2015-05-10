85f3aee0a80e14dc1a4a48f7f3adf968869070a9
aleksey savostin <aleksey.savostin@gmail.com>
2015-05-10 18:54:44 +0300

    stack entity corrected (2 bit operation aded)

--- stack.vhd
+++ stack.vhd
@@ -7,11 +7,9 @@ port(
 	 Reset: in std_logic; --Reset
     Enable: in std_logic;  --Enable the stack. Otherwise neither push nor pop will happen.
     Data_In: in std_logic_vector(size-1 downto 0);  --Data to be pushed to stack
-	 PUSH_barPOP: in std_logic;  --active low for POP and active high for PUSH.
-    Data_Out: out std_logic_vector(size-1 downto 0);  --Data popped from the stack.
-    Stack_Full: out std_logic;  --Goes high when the stack is full.
-    Stack_Empty: out std_logic  --Goes high when the stack is empty.
-    );
+	 Operation: in std_logic_vector(1 downto 0);
+    Data_Out1: out std_logic_vector(size-1 downto 0);
+	 Data_Out2: out std_logic_vector(size-1 downto 0));
 end stack;
 
 architecture Behavioral of stack is
@@ -19,59 +17,27 @@ architecture Behavioral of stack is
 type mem_type is array (RAMSize-1 downto 0) of std_logic_vector(size-1 downto 0);
 signal stack_mem : mem_type := (others => (others => '0'));
 signal stack_ptr : integer := RAMSize-1;
-signal full, empty : std_logic := '0';
 
 begin
-    Stack_Full <= full; 
-    Stack_Empty <= empty;
-    
     --PUSH and POP process for the stack.
-    process(Clk, Reset, PUSH_barPOP, Enable)
+    process(Clk, Reset, Operation, Enable)
     begin
-        if(rising_edge(Clk)) then
+        if(rising_edge(Clk) and Enable = '1') then
 		      if (Reset = '1') then
 				    stack_ptr <= RAMSize-1;
-					 full <= '0';
-					 empty <= '1';
-            end if;
-            --PUSH section.
-            if (Enable = '1' and PUSH_barPOP = '1' and full = '0') then
-                 --Data pushed to the current address.
-                stack_mem(stack_ptr) <= Data_In; 
-                if(stack_ptr /= 0) then
-                    stack_ptr <= stack_ptr - 1;
-                end if; 
-                --setting full and empty flags
-                if(stack_ptr = 0) then
-                    full <= '1';
-                    empty <= '0';
-                elsif(stack_ptr = RAMSize-1) then
-                    full <= '0';
-                    empty <= '1';
-                else
-                    full <= '0';
-                    empty <= '0';
-                end if;
-            end if;
-            --POP section.
-            if (Enable = '1' and PUSH_barPOP = '0' and empty = '0') then
-            --Data has to be taken from the next highest address(empty descending type stack).
-                if(stack_ptr /= RAMSize-1) then   
-                    Data_Out <= stack_mem(stack_ptr+1); 
-                    stack_ptr <= stack_ptr + 1;
-                end if; 
-                --setting full and empty flags
-                if(stack_ptr = 0) then
-                    full <= '1';
-                    empty <= '0';
-                elsif(stack_ptr = RAMSize-1) then
-                    full <= '0';
-                    empty <= '1';
-                else
-                    full <= '0';
-                    empty <= '0';
-                end if; 
-            end if;
+            elsif (Operation = "00") then --POP section.
+                stack_ptr <= stack_ptr + 1; --Data has to be taken from the next highest address(empty descending type stack).
+            elsif (Operation = "01") then --PUSH section.
+                stack_mem(stack_ptr) <= Data_In; --Data pushed to the current address.
+                stack_ptr <= stack_ptr - 1;
+            elsif (Operation = "10") then
+                stack_mem(stack_ptr+1) <= Data_In;
+				elsif (Operation = "11") then
+                stack_mem(stack_ptr+2) <= Data_In;
+					 stack_ptr <= stack_ptr + 1;
+            end if; 
         end if; 
     end process;
+	 Data_Out1 <= stack_mem(stack_ptr+1); 
+	 Data_Out2 <= stack_mem(stack_ptr+2);
 end Behavioral;
